bf0ebea82a2ade12ee93a7b446a2fe3a8a0860a2
TheRonv2 <boss.0057@bk.ru>
2023-11-14 22:41:10 +0300

    First commit

---
+++ TestVR/Assets/HandTracking.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandTracking : MonoBehaviour
+{
+    public UDPReceive udpReceive;
+    public GameObject[] handPoints;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string data = udpReceive.data;
+        data = data.Remove(0, 1);
+        data = data.Remove(data.Length - 1, 1);
+        print(data);
+        string[] points = data.Split(',');
+        print(points[0]);
+
+        for (int i = 0; i <= 32; i++)
+        {
+
+            float x = float.Parse(points[0 + (i * 3)]) / 100;
+            float y = float.Parse(points[1 + (i * 3)]) / 100;
+            float z = float.Parse(points[2 + (i * 3)]) / 300;
+
+            handPoints[i].transform.localPosition = new Vector3(x, y, z);
+
+        }
+    }
+}
